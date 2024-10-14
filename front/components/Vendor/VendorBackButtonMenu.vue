8591f16aab3aa0627088861fcab27a6f5b6c1973
fukuda <sfu983667@gmail.com>
2024-10-14 21:48:34 +0900

    vendorユーザ向け機能

---
+++ front/components/Vendor/VendorBackButtonMenu.vue
@@ -0,0 +1,53 @@
+<template>
+    <button @click="goBack" class="back__button">
+        <span class="line x"></span>
+        <span class="line y"></span>
+    </button>
+</template>
+
+<script setup>
+const router = useRouter()
+
+const goBack = () => {
+    router.back()
+}
+</script>
+
+<style scoped>
+.back__button {
+    width: 36px;
+    height: 36px;
+    background-color: #f4cb36; /* 背景色を青色に設定 */
+    border: none;
+    border-radius: 4px;
+    position: relative;
+    cursor: pointer;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3); /* ボタンの右下に影を追加 */
+    transition: background-color 0.3s; /* 背景色の変化にアニメーションを追加 */
+}
+
+.back__button:hover {
+    background-color: #b69b3a; /* ホバー時の背景色を設定 */
+}
+
+.line {
+    position: absolute;
+    background-color: white;
+}
+
+.x {
+    width: 22px;
+    height: 1px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(45deg);
+}
+
+.y {
+    width: 1px;
+    height: 22px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(45deg);
+}
+</style>
---
+++ front/layouts/vendor-menu.vue
@@ -0,0 +1,53 @@
+<template>
+        <header class="header">
+            <div class="header__container">
+                <VendorBackButtonMenu />
+            </div>
+        </header>
+        <main class="main">
+            <NuxtPage />
+        </main>
+        <footer class="footer"></footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+body {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    background-color: #ededed;
+}
+</style>
+
+<style scoped>
+.header__container {
+    display: flex;
+    margin: 40px auto;
+    padding: 0 20px;
+    max-width: 1130px;
+    width: 100%;
+    justify-content: space-between;
+    text-align: center;
+    box-sizing: border-box;
+}
+
+.main {
+    margin: 0 auto;
+    padding: 0 20px;
+    max-width: 1130px;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+.footer {
+    margin: 0 auto;
+    padding: 0 20px;
+    max-width: 1130px;
+    width: 100%;
+    box-sizing: border-box;
+}
+</style>
